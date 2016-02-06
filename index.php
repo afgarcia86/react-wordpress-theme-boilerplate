d8c349cafee3c7d3d328fa51e3f69ee2a196b52b
Andres <hello@andresthegiant.com>
2016-02-06 15:01:12 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Wordpress React Starter</title>
+  <link rel="stylesheet" href="./style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+  <div id="app"></div>
+  <script src="./index.js"></script>
+  <!-- Start Bootstrap -->
+  <script src="/vendor/tether/dist/js/tether.min.js"></script>
+  <script src="/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- End Bootstrap -->
+</body>
+</html>
